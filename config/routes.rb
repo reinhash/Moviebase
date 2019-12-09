044571e2bf247a7eaa0bbd52bc30e1f8a4bb5364
reinhash <reinhard.scheuerle@gmail.com>
2019-12-09 14:48:55 -0500

    sliders are embedded in form

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
+  get "/home", to: "home#index"
   get "/declaration", to: "declaration#index"
+  get "/random_movie", to: "home#random_movie"
 end
